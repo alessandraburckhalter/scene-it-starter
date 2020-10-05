8c81e90b1a31f5e783b2bf419bcaa8f35e5d226b
Alessandra Burckhalter <ale.lptc@gmail.com>
2020-10-05 09:17:55 -0400

    new layout

--- index.js
+++ index.js
@@ -1,55 +1,83 @@
-function saveToWatchList(imdbID) {
-    const movie = movieData.find((currentMovie) => {
-        return currentMovie.imdbID == imdbID;
-});
+let movieData;
+// function that takes in the movie we want to add to our localStorage
+function addMovieToLocalStorage(movie, imdbID){
 
-const watchlistJSON =  localStorage.getItem('watchlist')
-const watchlist = JSON.parse(watchlistJSON);
+  let watchListJSON = localStorage.getItem('watchList');
+  let watchList = JSON.parse(watchListJSON);
+  
+  // checks if watchList exists, if it doesn't, we initiate the empty array.
+  if (!watchList){watchList = [];}
+  const isMovieInListAlready = watchList.find((watchListMovie)=> {
+    // checks if there is one first, then if they are the same.
+    return watchListMovie.imdbID && watchListMovie.imdbID == imdbID;
+  })
+  if (!isMovieInListAlready){
+    watchList.push(movie);
+  }
 
-if (watchlist == null) {
-    watchlist = [];
+  watchListJSON = JSON.stringify(watchList);
+
+  localStorage.setItem('watchList', watchListJSON)
+
 }
 
-watchlist.push(movie);
-watchlistJSON = JSON.stringify(watchlist);
-localStorage.setItem('watchlist', watchlistJSON);
+
+
+// function to add a movie to our local storage if the user clicks the add button
+function saveToWatchList(imdbID){
+  const movie = movieData.find((currentMovie) =>{
+    return currentMovie.imdbID == imdbID;
+  });
+
+  addMovieToLocalStorage(movie, imdbID);
+
+  console.log(localStorage.getItem('watchList'));
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+function deleteFromWatchList(){
+  localStorage.removeItem('watchList')
+}
+
     function renderMovies(movieArray) {
         const movieHtmlArray = movieArray.map((currentMovie) => {
             //console.log(currentMovie.title)
-            return `<div class="movie card-body col-md-4 ">
-            <img src="${currentMovie.Poster}" class="poster" alt="" width="100%" height="85%"><br>
-            <span class="card-title">${currentMovie.Title}</span><br>
-            <span class="card-text">${currentMovie.Year}</span><br>
-            <button type="button" class="btn btn-warning"  onclick="saveToWatchList('${currentMovie.imdbID}')">Add</button>
-            </div> `    
+            return `<div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
+            <div class="card align-items-left only-card style="height: 100vh;">
+            <img src="${currentMovie.Poster}" class="card-img-top " alt="" width="100%" height="400">
+            <span class="card-title mt-2">${currentMovie.Title}</span>
+            <span class="card-text mb-2">${currentMovie.Year}</span>
+            <button type="button" class="btn btn-warning" id="addButton" onclick="saveToWatchList('${currentMovie.imdbID}')">Add movie</button>
+            </div> 
+            </div>`    
 
         })
         return movieHtmlArray.join('');    
     }
 
-    // Make the movies appear when search button is clicked
-    const searchForm = document.querySelector("#search-form");
-    searchForm.addEventListener('submit', (e) => {
-        e.preventDefault();
-        const container = document.querySelector('.movie-container');
-        container.innerHTML = renderMovies(movieData);
-
-        const searchString = document.getElementsByClassName('form-control')[0].value;
-
-        console.log(searchString);
-        
-        const urlEncodedSearchString = encodeURIComponent(searchString);
-        axios.get("http://www.omdbapi.com/?apikey=59354c85&s=" + urlEncodedSearchString)
-        .then(response => {
-        container.innerHTML = renderMovies(response.data.Search);
-        movieData = response.data.Search;
-        console.log(urlEncodedSearchString);
-
-        });
-    })
     
-})
 
+    document.addEventListener('DOMContentLoaded', function(){
+        document.addEventListener('click', (e)=>{
+            if(e.target.id !== 'navLink'){
+              e.preventDefault();
+              let searchString = $('.search-bar').val();
+              let urlEncodedSearchString = encodeURIComponent(searchString);
+              if(e.target.id === 'search'){
+                  const movieContainer = document.querySelector('.movie-container');
+                  axios.get("http://www.omdbapi.com/?apikey=59354c85&s=" + urlEncodedSearchString)
+                    .then((response)=>{
+                      console.log(response.data.Search);
+                      movieContainer.innerHTML = renderMovies(response.data.Search);
+                      movieData = response.data.Search;
+                    })
+              }else if(e.target.id === 'addButton'){
+                e.target.style.backgroundColor  = '#ac4b1c';
+                e.target.textContent = "Added to your watchlist";
+              }
+            }
+        
+        })
+
+        
+        
+    })
--- watchlist.js
+++ watchlist.js
@@ -1,21 +1,61 @@
-const watchlistJson =  localStorage.getItem('watchlist')
-const watchlist = JSON.parse(watchlistJson);
-
-//document.addEventListener('DOMContentLoaded', function() {
-    function renderMovies(list) {
-        const movieHtml = list.map((currentMovie) => {
-            //console.log(currentMovie.title)
-            return `<div class="movie card-body col-md-4 ">
-            <img src="${currentMovie.Poster}" class="poster" alt="" width="100%" height="85%"><br>
-            <span class="card-title">${currentMovie.Title}</span><br>
-            <span class="card-text">${currentMovie.Year}</span><br>
-            <button type="button" class="btn btn-warning"  onclick="saveToWatchList('${currentMovie.imdbID}')">Add</button>
-            </div> `    
-
-        })
-        return movieHtml.join('');    
-    }
-
-    const container = document.querySelector(".movie-container");
-    container.innerHTML = renderMovies(watchlist);
+const pullFromLocal = ()=> {
+    let watchListJSON = localStorage.getItem('watchList');
+    let watchList = JSON.parse(watchListJSON);
     
+    // checks if watchList exists, if it doesn't, we initiate the empty array.
+    if (!watchList){watchList = [];}
+    return watchList;
+}
+
+// function that takes in the movie we want to add to our localStorage
+function removeMovieFromLocalStorage(watchList, movie){
+    let deleteIndex;
+
+    for (let index = 0; index < watchList.length; index++){
+        if(watchList[index].Title === movie.Title){
+            deleteIndex = index;
+        }
+    }
+    watchList.splice(deleteIndex, 1);
+    let watchListJSON = JSON.stringify(watchList);
+    localStorage.setItem('watchList', watchListJSON)
+    return watchList;
+  }
+
+function deleteFromWatchList(imdbID){
+    // does the opposite from add to watchlist
+    let localList = pullFromLocal();
+    console.log(localList);
+    const movie = localList.find((currentMovie) =>{
+        return currentMovie.imdbID == imdbID;
+    });
+
+    localList = removeMovieFromLocalStorage(localList, movie);
+
+    const movieContainer = document.querySelector('.movie-container');
+    movieContainer.innerHTML = renderMovies(localList);    
+}
+
+
+function renderMovies(movieArray){
+    const movieHtmlArray = movieArray.map((currentMovie) => {
+        
+        return `
+        <div class="movie col-12 col-sm-6 col-md-6 col-lg-4 mb-3">
+        <div class="card align-items-left only-card" style="height: auto">
+        <img src="${currentMovie.Poster}" class="card-img-top " alt="" width="100%" height="400">
+        <span class="card-title mt-2">${currentMovie.Title}</span>
+        <span class="card-text mb-2">${currentMovie.Year}</span>
+            <button class="btn btn-primary addButton" id="removeButton" onClick="deleteFromWatchList('${currentMovie.imdbID}')">Remove movie</button>
+          </div>
+        </div>
+     
+                `           
+    });
+    return movieHtmlArray.join('');                   
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+    const movieContainer = document.querySelector('.movie-container');
+    movieContainer.innerHTML = renderMovies(pullFromLocal());
+})
